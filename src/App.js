f92b47b9d402230f4cb497bfc7ae8b23621bef89
alaahamouly <a.hamouly@evorezm.com>
2020-07-15 16:07:24 +0200

    push todo to github

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import TodoItems from './components/TodoItems/TodoItems'
+import AddItem from './components/AddItem/AddItem'
+
+
+class App extends Component {
+  state = {
+    items: [
+      {id:1,name:"Alaa",age:25},
+      {id:2,name:"Zeyad",age:25},
+      {id:3,name:"Ahmed",age:24},
+      {id:4,name:"Mahmoud",age:24}
+    ]
+  }
+
+  deleteItem = (id) => {
+    let items = this.state.items.filter(item => {
+      return item.id !== id
+    })
+    this.setState({items})
+  }
+
+  addItem = (item) => {
+    item.id= Math.random();
+    let items = this.state.items;
+    items.push(item);
+    this.setState({items})
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="text-center">Todo List</h1>
+        <TodoItems items={this.state.items} deleteItem={this.deleteItem} />
+        <AddItem addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default App;
